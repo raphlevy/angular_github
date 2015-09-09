d1685c645a03e8262a1151b6b317d86cfd241f8d
raphlevy <raphlevy.perso@gmail.com>
2015-09-09 11:56:35 +0100

    Added search factory

--- js/GitUserSearchController.js
+++ js/GitUserSearchController.js
@@ -1,14 +1,12 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
 
   var self = this;
 
-  var searchResource = $resource('https://api.github.com/search/users/');
-
   self.doSearch = function() {
-    self.searchResult = searchResource.get(
-      { q: self.searchTerm,
-        access_token: githubToken}
-    );
+    Search.query(self.searchTerm)
+      .then(function(response) {
+        self.searchResult = response.data;
+      })
   };
 
 }]);
